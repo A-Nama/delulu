89baa1ff754e7879befecf0c3fcbb6c9aff420a4
aishanama2015 <aishanama2015@gmail.com>
2024-09-28 15:12:20 +0530

    Added basic code structure

---
+++ app/safespaces.py
@@ -0,0 +1,6 @@
+def get_safespaces():
+    return [
+        {"name": "Forest Retreat", "type": "image", "url": "static/images/forest.jpg"},
+        {"name": "Beach Vibes", "type": "image", "url": "static/images/beach.jpg"},
+        {"name": "Calming River", "type": "video", "url": "static/videos/river.mp4"}
+    ]
