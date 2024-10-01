0a14ae11d195d3b5a5a70ef8f260d66b16d80e37
Anjana <anjanasankar50@gmail.com>
2024-10-01 23:50:49 +0530

    i love pinterest

--- app/safespaces.py
+++ app/safespaces.py
@@ -2,5 +2,11 @@ def get_safespaces():
     return [
         {"name": "Forest Retreat", "type": "image", "url": "https://c4.wallpaperflare.com/wallpaper/464/132/911/worms-eye-view-nature-studio-ghibli-sunlight-wallpaper-preview.jpg"},
         {"name": "Floating through the sky", "type": "image", "url": "https://c4.wallpaperflare.com/wallpaper/536/579/589/nausicaa-of-the-valley-of-the-wind-studio-ghibli-wallpaper-preview.jpg"},
-        {"name": "Meadow of Peace", "type": "image", "url": "https://c4.wallpaperflare.com/wallpaper/70/752/1009/studio-ghibli-my-neighbor-totoro-totoro-anime-girls-wallpaper-preview.jpg"}
-    ]
+        {"name": "Meadow of Peace", "type": "image", "url": "https://c4.wallpaperflare.com/wallpaper/70/752/1009/studio-ghibli-my-neighbor-totoro-totoro-anime-girls-wallpaper-preview.jpg"},
+        {"name": "fly away with me","type": "image", "url": "https://c4.wallpaperflare.com/wallpaper/707/91/650/kikis-delivery-service-studio-ghibli-wallpaper-preview.jpg"},
+        {"name": "lily field", "type": "image", "url": "https://i.pinimg.com/564x/f1/e0/a7/f1e0a7a79352c0aac4a88bfd5894a759.jpg"},
+        {"name": "evening walk", "type": "image", "url": "https://i.pinimg.com/564x/15/3f/b1/153fb13af7d4cdbb90b8d79607a1a6b8.jpg"},
+        {"name": "small town", "type": "image", "url": "https://i.pinimg.com/564x/ac/5b/f2/ac5bf2df2c0b8d1b0063eaa3b9ddbfc9.jpg"},
+        {"name": "moonlight", "type": "image", "url": "https://i.pinimg.com/736x/52/4f/ed/524fedd146244d2420b70cffb1542bd6.jpg"},
+        {"name": "citynight", "type": "image", "url": "https://i.pinimg.com/564x/71/0f/92/710f92679cfca2eaec83f8c10f0cdcd8.jpg"},
+        {"name": "greek summer", "type": "image", "url": "https://i.pinimg.com/564x/68/5e/b0/685eb0a58fb6d60d2ceaaf2ab9fe2b77.jpg"}]
