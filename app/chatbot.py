335b98d621bb20f6dcd3812f9aea4207d59bed2e
A_Nama <145112688+A-Nama@users.noreply.github.com>
2024-10-02 22:01:08 +0530

    Update chatbot.py
    Updated to host on cloud

--- app/chatbot.py
+++ app/chatbot.py
@@ -1,9 +1,9 @@
 import os
 import streamlit as st
 import google.generativeai as genai
-from dotenv import load_dotenv
+# if running locally include:- from dotenv import load_dotenv
 
-load_dotenv()
+
 
 # Configure the Gemini API
 genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
